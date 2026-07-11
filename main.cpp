0f2c90323b4ec0db32ff47b4c86c7d96f51f60df
Alexander Stoilov <aleksandarstoilov26@gmail.com>
2026-07-12 00:40:48 +0300

    Added numbers 0-9 to the default character set

--- main.cpp
+++ main.cpp
@@ -17,7 +17,8 @@ int main(int argc, char* argv[]){
     const std::vector<char> ascii_set = {
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '`', '{', '|', '}', '~', 
+    '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '`', '{', '|', '}', '~',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
     };
 
     const std::vector<char> lowercase = {
@@ -43,7 +44,7 @@ int main(int argc, char* argv[]){
     std::vector<char> custom_char_set = {};
 
 
-    std::uniform_int_distribution<int> dist(0,81);
+    std::uniform_int_distribution<int> dist(0,89);
     std::uniform_int_distribution<int> custom_dist(0,0);
     
     int choose_character_set;
